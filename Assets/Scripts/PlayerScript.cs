6489a3cdc2a6fe325485d8d3f5e2aa472baedf17
Jonathan Alexander <notdisliked@gmail.com>
2015-12-05 22:03:36 -0800

    Fix Player movement to be not janky

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,8 +6,7 @@ public class PlayerScript : MonoBehaviour {
     [HideInInspector]
     public bool jump = false;
 
-    public float acceleration = 365f;
-    public float maxSpeed = 5f;
+    public float speed = 5f;
     public float jumpPower = 150f;
     public Transform groundCheck;
 
@@ -22,25 +21,14 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.DrawLine(transform.position, groundCheck.position, Color.yellow);
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Platform"));
 
-        if (Input.GetButtonDown("Jump") && grounded) {
-            jump = true;
-        }
-    }
-
-    void FixedUpdate() {
         float h = Input.GetAxis("Horizontal");
 
-        //if (h * rb2d.velocity.x < maxSpeed)
-            rb2d.AddForce(Vector2.right * h * acceleration);
+        rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb2d.velocity.y);
 
-        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-
-        if (jump) {
-            rb2d.AddForce(new Vector2(0f, jumpPower));
+        if (Input.GetButtonDown("Jump") && grounded) {
+            rb2d.AddForce(Vector2.up * jumpPower);
             jump = false;
         }
     }
