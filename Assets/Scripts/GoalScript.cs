64aa50e1099398be28f93713b91222b6d68035df
Tin Huynh <tinlun123@gmail.com>
2016-03-15 16:36:13 -0700

    Added level transition
    More progress on #13
    Added a goal sprite and level transition when touching the goal.

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "Player"){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
