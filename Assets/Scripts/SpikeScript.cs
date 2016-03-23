aeb1fcb06b55a40a1a263a1d27c2948cba6fdcef
Tin Huynh <tinlun123@gmail.com>
2016-03-23 15:37:12 -0700

    Added spikes
    #10 Spikes kill you.

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SpikeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag == "Player"){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
