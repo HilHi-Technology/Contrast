b7f3322c0aa72d9238fea421ecbe3234f7a180f8
Jonathan Alexander <notdisliked@gmail.com>
2016-04-05 15:50:14 -0700

    Adjust other world colors

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -6,14 +6,14 @@ public class GoalScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	    
+
 	}
-    
+
     void OnTriggerEnter2D(Collider2D col){
         if(col.tag == "Player"){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
