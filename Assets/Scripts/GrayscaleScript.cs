fc14ee3c099029683b1303028917acdb9392fca9
Tin Huynh <tinlun123@gmail.com>
2016-04-05 16:44:55 -0700

    Grayscale script
    :)))))))))

---
+++ Assets/Scripts/GrayscaleScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrayscaleScript : MonoBehaviour {
+    public float saturation;
+    private Renderer renderer;
+	// Use this for initialization
+	void Start () {
+	    renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    renderer.material.SetFloat("_EffectAmount", saturation);
+	}
+}
