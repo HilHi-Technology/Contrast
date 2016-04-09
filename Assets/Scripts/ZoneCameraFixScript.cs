3aa86227b858f8cefb5ed3420851b0abf012e119
Tin Huynh <tinlun123@gmail.com>
2016-04-09 15:54:57 -0700

    Added zone animation and boundaries

---
+++ Assets/Scripts/ZoneCameraFixScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneCameraFixScript : MonoBehaviour {
+
+    private Camera camera;
+    public Transform quad;
+    
+	// Use this for initialization
+	void Start () {
+	    camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    camera.orthographicSize = transform.parent.localScale.x / 2;
+	}
+}
